f76351aa26490ccbbd0964b86113121634f2b275
Richard Smith <richard@flwless.com>
2017-06-19 09:53:44 +0200

    Added today extension

---
+++ push4help/TodayViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TodayViewController.swift
+//  push4help
+//
+//  Created by Richard Smith on 2017-06-15.
+//  Copyright © 2017 Richard Smith. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+        extensionContext?.widgetLargestAvailableDisplayMode = .expanded
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    @IBAction func enterTheApp(_ sender: Any) {
+        extensionContext?.open(<#T##URL: URL##URL#>, completionHandler: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
+        
+    }
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+        let expanded = activeDisplayMode == .expanded
+        preferredContentSize = expanded ? CGSize(width: maxSize.width, height: 200) : maxSize
+        
+        
+    }
+        
+}
